6862d8ab436ccbef5dae0f63bd54418309abb9b2
rafael.detilio <rafael.detilio@senior.com.br>
2025-04-14 22:28:41 -0300

    Criação da classe user e comunicação com banco de dados mysql.

---
+++ src/main/java/br/com/fiap/tech/challenge/erp_restaurant/model/user.java
@@ -0,0 +1,82 @@
+package br.com.fiap.tech.challenge.erp_restaurant.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+public class user {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String login;
+    private Date dateChange;
+    private Date dateGeneration;
+    private String adress;
+
+    public user(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Date getDateChange() {
+        return dateChange;
+    }
+
+    public void setDateChange(Date dateChange) {
+        this.dateChange = dateChange;
+    }
+
+    public Date getDateGeneration() {
+        return dateGeneration;
+    }
+
+    public void setDateGeneration(Date dateGeneration) {
+        this.dateGeneration = dateGeneration;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+}
