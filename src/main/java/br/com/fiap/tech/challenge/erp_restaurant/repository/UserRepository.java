cd49c26333cf51582854a64da7bb88da129b5542
rafael.detilio <rafael.detilio@senior.com.br>
2025-04-28 10:58:01 -0300

    Implementação dos métodos para alterar, deletar e criar usuário.

--- src/main/java/br/com/fiap/tech/challenge/erp_restaurant/repository/UserRepository.java
+++ src/main/java/br/com/fiap/tech/challenge/erp_restaurant/repository/UserRepository.java
@@ -3,6 +3,8 @@ package br.com.fiap.tech.challenge.erp_restaurant.repository;
 import br.com.fiap.tech.challenge.erp_restaurant.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmailIgnoreCase(String email);
 }
--- src/main/java/br/com/fiap/tech/challenge/erp_restaurant/service/UserService.java
+++ src/main/java/br/com/fiap/tech/challenge/erp_restaurant/service/UserService.java
@@ -5,11 +5,18 @@ import br.com.fiap.tech.challenge.erp_restaurant.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
     private UserRepository userRepository;
+    private Optional<User> userFound;
+    private LocalDate dataAtual = LocalDate.now();
+    Date date = Date.from(dataAtual.atStartOfDay(ZoneId.systemDefault()).toInstant());
 
     @Autowired
     public UserService(UserRepository userRepository){
@@ -18,6 +25,53 @@ public class UserService {
 
     public List<User> listUsers() {
         return userRepository.findAll();
+    }
+
+    public Optional<User> userSearch (String email) {
+        return userRepository.findByEmailIgnoreCase(email);
+    }
+
+    public void userValidation(String email) {
+        userFound = userSearch(email);
+    }
+
+    public User registerUser(String name, String login, String password, String email) {
+        userValidation(email);
+        if(userFound.isPresent()){
+            throw new IllegalArgumentException("Usuário já cadastrado para este e-mail!");
+        } else {
+            User user = new User();
+            user.setName(name);
+            user.setLogin(login);
+            user.setPassword(password);
+            user.setEmail(email);
+            user.setDateGeneration(date);
+            return userRepository.save(user);
+        }
+    }
+
+    public User updateUser(String name, String login, String password, String email) {
+        userValidation(email);
+        if(userFound.isPresent()){
+            User user = userFound.get();
+            user.setName(name);
+            user.setLogin(login);
+            user.setPassword(password);
+            user.setDateChange(date);
+            return userRepository.save(user);
+        } else {
+            throw new IllegalArgumentException("Não existe usuário cadastrado para este e-mail!");
+        }
+    }
+
+    public String deleteUser(String email){
+        userValidation(email);
+        if(userFound.isPresent()){
+            userRepository.delete(userFound.get());
+            return "Usuário excluído com sucesso!";
+        } else {
+            throw new IllegalArgumentException("Não existe usuário cadastrado para este e-mail!");
+        }
     }
 
 }
