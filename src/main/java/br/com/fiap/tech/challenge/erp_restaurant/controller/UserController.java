ef2386cd5cde28ffee32d8179d07f832cb694cca
rafael.detilio <rafael.detilio@senior.com.br>
2025-04-28 10:58:01 -0300

    Implementação das classes de endereço.

--- src/main/java/br/com/fiap/tech/challenge/erp_restaurant/controller/UserController.java
+++ src/main/java/br/com/fiap/tech/challenge/erp_restaurant/controller/UserController.java
@@ -3,12 +3,13 @@ package br.com.fiap.tech.challenge.erp_restaurant.controller;
 import br.com.fiap.tech.challenge.erp_restaurant.model.User;
 import br.com.fiap.tech.challenge.erp_restaurant.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -24,5 +25,41 @@ public class UserController {
     public ResponseEntity<List<User>> listUsers(){
         List<User> users = userService.listUsers();
         return ResponseEntity.ok(users);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        try {
+            User newUser = userService.registerUser(user.getName(), user.getLogin(), user.getPassword(), user.getEmail());
+            return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+        } catch (IllegalArgumentException e) {
+            Map<String, String> resposta = new HashMap<>();
+            resposta.put("mensagem", e.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(resposta);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody User user){
+        try {
+            User updateUser = userService.updateUser(user.getName(), user.getLogin(), user.getPassword(), user.getEmail());
+            return ResponseEntity.ok(updateUser);
+        } catch (IllegalArgumentException e) {
+            Map<String, String> resposta = new HashMap<>();
+            resposta.put("mensagem", e.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(resposta);
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteUser(@RequestBody User user){
+        try {
+            String email = userService.deleteUser(user.getEmail());
+            return ResponseEntity.ok(email);
+        } catch (IllegalArgumentException e) {
+            Map<String, String> resposta = new HashMap<>();
+            resposta.put("mensagem", e.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(resposta);
+        }
     }
 }
